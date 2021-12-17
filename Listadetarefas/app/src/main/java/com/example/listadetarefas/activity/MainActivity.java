56f5e1cbd7edb1285961270d9583192f2d60627d
dheboraalice <42244543+dheboraalice@users.noreply.github.com>
2021-12-17 09:40:53 -0400

    Atividade

--- Listadetarefas/app/src/main/java/com/example/listadetarefas/activity/MainActivity.java
+++ Listadetarefas/app/src/main/java/com/example/listadetarefas/activity/MainActivity.java
@@ -1,19 +1,32 @@
-package com.example.listadetarefas;
+package com.example.listadetarefas.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.listadetarefas.R;
+import com.example.listadetarefas.adapter.TarefaAdapter;
+import com.example.listadetarefas.model.Tarefa;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private TarefaAdapter tarefaAdapter;
+    private List<Tarefa> listaTarefas = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +34,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //Configurar recucler
+        recyclerView = findViewById(R.id.recyclerView);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -32,6 +48,34 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    public void carregarListaTarefas(){
+
+        //Listar tarefas
+        Tarefa tarefa1 = new Tarefa();
+        tarefa1.setNomeTarefa("Ir ao mercado");
+        listaTarefas.add(tarefa1);
+
+        Tarefa tarefa2 = new Tarefa();
+        tarefa1.setNomeTarefa("Ir a feira");
+        listaTarefas.add(tarefa2);
+
+        //Exibi listqa de tarefas no Recyclerview
+
+
+
+        //Configurar um adapter
+            tarefaAdapter = new TarefaAdapter(listaTarefas);
+
+
+        //Configurar Recyclerview
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.addItemDecoration(new DividerItemDecoration(getApplicationContext(), LinearLayout.VERTICAL));
+        //recyclerView.setAdapter();
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
