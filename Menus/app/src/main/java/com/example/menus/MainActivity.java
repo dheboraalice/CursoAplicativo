80fbf7650ef13004ecfa2cafad5493fe488073c1
dheboraalice <42244543+dheboraalice@users.noreply.github.com>
2021-12-17 14:44:12 -0400

    Criando Menu

---
+++ Menus/app/src/main/java/com/example/menus/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.menus;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.itemSalvar :
+                Toast.makeText(MainActivity.this, "Item salvo", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.itemEditar :
+                Toast.makeText(MainActivity.this, "Item editado", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.itemConfiguracao :
+                Toast.makeText(MainActivity.this, "Item configurado", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
